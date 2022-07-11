07cab3e1e1a8d63421c68f9f73e8fac0c6eeb786
Andres <andres.rondan.next@bbva.com>
2022-07-11 15:30:10 +0200

    fix(pagination): fix en estilos y en el componente sobre el que se hace scroll

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -5,23 +5,15 @@ export const InfiniteScroll = ({ children, onPaginate, currentPage }) => {
 	const scrollingLayer = useRef();
 
 	const paginationHandler = ( { target }) => {
-		const ZERO = document.getElementsByTagName('body')[0].clientHeight;
-		const { scrollingElement } = target;
-			console.log(scrollingElement.scrollHeight)
-		if (scrollingElement.scrollHeight - scrollingElement.scrollTop <= ZERO) {
+		const ZERO = target.clientHeight;
+
+		if (target.scrollHeight - target.scrollTop <= ZERO) {
 			onPaginate({
 				page: currentPage.page +1,
 				limit: currentPage.limit
 			});
 		}
 	}
-
-	// _useEffect(() => {
-	// 	document.addEventListener('scroll', paginationHandler);
-	// 	return () => {
-	// 		document.removeEventListener('scroll', paginationHandler);
-	// 	};
-	// }, [currentPage]);
 
 	useEffect(() => {
 		scrollingLayer.current.addEventListener('scroll', paginationHandler);
